bd52c9c46f63e73a34d6262ad66c0839d8245684
DN <dilaran@live.com>
2020-03-05 22:26:28 -0800

    Updates

---
+++ precache-manifest.0d051ce1c63a344f09e409dde0fca3d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3947f9c26ff91a6647c2979ef106391d",
+    "url": "/Portfolio-react/index.html"
+  },
+  {
+    "revision": "4f0b547e293b9d08e546",
+    "url": "/Portfolio-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "87aba6e6ba7a15a84ef7",
+    "url": "/Portfolio-react/static/js/2.539a8a2c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Portfolio-react/static/js/2.539a8a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f0b547e293b9d08e546",
+    "url": "/Portfolio-react/static/js/main.10fd800f.chunk.js"
+  },
+  {
+    "revision": "a8e392039e26c27d47bf",
+    "url": "/Portfolio-react/static/js/runtime-main.6dcd7e8e.js"
+  }
+]);
